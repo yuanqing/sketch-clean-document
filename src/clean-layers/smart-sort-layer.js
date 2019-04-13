26d8388cd7504056e241a24415d5473f85978f6a
yuanqing <desk@yuanqing.sg>
2019-04-14 04:14:54 +0800

    Fix import path

--- src/clean-layers/smart-sort-layer.js
+++ src/clean-layers/smart-sort-layer.js
@@ -1,5 +1,5 @@
 import { smartSort } from 'sketch-sort-layer-list/src/attributes/smart-sort'
-import { updateLayerList } from 'sketch-sort-layer-list/src/sort-layers'
+import updateLayerList from 'sketch-sort-layer-list/src/update-layer-list'
 
 export default function smartSortLayer (layer) {
   if (layer.type === 'Artboard' || layer.type === 'Group') {
