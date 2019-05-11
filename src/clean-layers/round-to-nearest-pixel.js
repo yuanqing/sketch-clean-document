7652acbac38339f2926fe00397e2187c100b5e2e
yuanqing <desk@yuanqing.sg>
2019-05-11 12:38:43 +0800

    Adjust parent groups

--- src/clean-layers/round-to-nearest-pixel.js
+++ src/clean-layers/round-to-nearest-pixel.js
@@ -1,7 +1,10 @@
+import { adjustParentGroupsToFit } from 'sketch-plugin-helper'
+
 const keys = ['width', 'height', 'x', 'y']
 
 export default function roundToNearestPixel (layer) {
-  keys.forEach(function(key) {
+  keys.forEach(function (key) {
     layer.frame[key] = Math.round(layer.frame[key])
+    adjustParentGroupsToFit(layer)
   })
 }
